0f0f7e2642fed19d75a87df99ab4517535e631c6
KhaiNguyenHoang <khaiproject1234@gmail.com>
2025-08-29 22:51:39 +0700

    Update

---
+++ lua/polish.lua
@@ -0,0 +1,15 @@
+-- Set up custom filetypes
+vim.filetype.add {
+  extension = { rasi = "rasi", rofi = "rasi", wofi = "rasi" },
+  filename = {
+    ["vifmrc"] = "vim",
+  },
+  pattern = {
+    [".*/waybar/config.*"] = "jsonc",
+    [".*/mako/config"] = "dosini",
+    [".*/kitty/.+%.conf"] = "kitty",
+    [".*/hypr/.+%.conf"] = "hyprlang",
+    [".*/sway/.+%.conf"] = "swayconfig",
+    ["%.env%.[%w_.-]+"] = "sh",
+  },
+}
